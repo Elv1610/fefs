63042c97ad3e2fe59087693d0a72ba66f4c8ccc3
Elvis Ciuffetelli <elvisciuffetelli@MacBook-Pro-di-Elvis.local>
2020-02-03 22:15:20 +0100

    changed text in node app console log

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+app.get("/", (req, res) => {
+  res.send("Hello from node app");
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
-
