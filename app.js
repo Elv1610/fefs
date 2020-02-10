5f4be78631d6556a59acab16ad85179021bddb2f
Elvis Ciuffetelli <elvisciuffetelli@MacBook-Pro-di-Elvis.local>
2020-02-10 20:03:42 +0100

    added demo path

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello from node app");
 });
 
+app.get("/demo", (req, res) => {
+  res.set("X-full-stack-custom", "4life");
+  res.status(418);
+  res.send("I prefer coffee");
+});
+
 app.listen(port, () => console.log(`App listening on port ${port}!`));
